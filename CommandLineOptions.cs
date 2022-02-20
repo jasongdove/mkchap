f1add1f83867529589e529d681704ad7659d38c6
Jason Dove <jason@jasondove.me>
2022-02-19 20:47:00 -0600

    detect chapters, add options

--- CommandLineOptions.cs
+++ CommandLineOptions.cs
@@ -6,4 +6,13 @@ public class CommandLineOptions
 {
     [Value(index: 0, Required = true, HelpText = "Input file")]
     public string Input { get; set; }
+
+    [Option(shortName: 's', longName: "seconds", Required = false, Default = 1.0d)]
+    public double MinBlackSeconds { get; set; }
+
+    [Option(shortName: 'r', longName: "ratio", Required = false, Default = 0.9d)]
+    public double RatioBlackPixels { get; set; }
+
+    [Option(shortName: 'b', longName: "black", Required = false, Default = 0.1d)]
+    public double BlackPixelThreshold { get; set; }
 }
--- Program.cs
+++ Program.cs
@@ -14,17 +14,26 @@ public static class Program
                 {
                     try
                     {
-                        // TODO: get duration
-                        // TODO: get chapters
                         var maybeDuration = await GetDuration(opts.Input);
-                        Console.WriteLine(maybeDuration);
-                        return 0;
+                        foreach (var duration in maybeDuration)
+                        {
+                            var blackDetectOutput = await BlackDetect(opts.Input, opts.MinBlackSeconds, opts.RatioBlackPixels,
+                                opts.BlackPixelThreshold);
+                            var blackSections = GetBlackSections(blackDetectOutput, opts.MinBlackSeconds);
+                            foreach (var section in blackSections)
+                            {
+                                Console.WriteLine(section);
+                            }
+
+                            return 0;
+                        }
                     }
                     catch (Exception ex)
                     {
                         Console.WriteLine(ex);
-                        return -2;
                     }
+
+                    return -2;
                 },
                 _ => Task.FromResult(-1));
 
@@ -44,6 +53,30 @@ public static class Program
         }
 
         return None;
+    }
+
+    private static async Task<string> BlackDetect(string inputFile, double minBlackSeconds, double ratioBlackPixels,
+        double blackPixelThreshold)
+    {
+        // rework filename in a format that works on windows
+        if (OperatingSystem.IsWindows())
+        {
+            // \ is escape, so use / for directory separators
+            inputFile = inputFile.Replace(@"\", @"/");
+
+            // colon after drive letter needs to be escaped
+            inputFile = inputFile.Replace(@":/", @"\:/");
+        }
+
+        return await GetFFprobeOutput(new List<string>
+        {
+            "-f", "lavfi",
+            "-i",
+            $"movie={inputFile},blackdetect=d={minBlackSeconds}:pic_th={ratioBlackPixels}:pix_th={blackPixelThreshold}[out0]",
+            "-show_entries", "frame_tags=lavfi.black_start,lavfi.black_end",
+            "-of", "default=nw=1",
+            "-v", "panic"
+        });
     }
 
     private static async Task<string> GetFFprobeOutput(IEnumerable<string> arguments)
@@ -73,4 +106,23 @@ public static class Program
 
         return await process.StandardOutput.ReadToEndAsync();
     }
+
+    private static List<BlackSection> GetBlackSections(string blackDetectOutput, double minBlackSeconds) =>
+        blackDetectOutput.Split("\n")
+            .Map(s => s.Trim())
+            .Filter(s => !string.IsNullOrWhiteSpace(s))
+            .Map(s => TimeSpan.FromSeconds(double.Parse(s.Split("=")[1], NumberStyles.Number,
+                NumberFormatInfo.InvariantInfo)))
+            .Chunk(2)
+            .Filter(c => c.Length == 2)
+            .Map(c => new BlackSection(c[0], c[1]))
+            .Filter(bs => bs.Start.TotalSeconds >= minBlackSeconds && bs.Duration.TotalSeconds >= minBlackSeconds)
+            .ToList();
+
+    private record BlackSection(TimeSpan Start, TimeSpan Finish)
+    {
+        public TimeSpan Duration => Finish - Start;
+    };
+
+    private record Chapter(TimeSpan Start, TimeSpan Finish);
 }
