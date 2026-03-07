86df2dcfd915e395555fe87b592bc289e1de3560
Jason Dove <1695733+jasongdove@users.noreply.github.com>
2026-03-07 08:15:32 -0600

    basic command line in rust

---
+++ src/main.rs
@@ -0,0 +1,69 @@
+mod window;
+
+use clap::Parser;
+use window::{Window, window_parser};
+
+#[derive(Debug, Parser)]
+struct Args {
+    input: String,
+
+    #[arg(
+        short = 's',
+        long = "seconds",
+        help = "minimum number of seconds the video must be black",
+        default_value_t = 1.0
+    )]
+    min_black_seconds: f64,
+
+    #[arg(
+        short = 'r',
+        long = "ratio",
+        help = "ratio (0 to 1) of black pixels to non-black pixels to treat a frame as black",
+        default_value_t = 0.9,
+        value_parser = black_pixel_ratio_in_range
+    )]
+    ratio_black_pixels: f64,
+
+    #[arg(
+        short = 'b',
+        long = "black",
+        help = "maximum luminance (0 to 1) for which a pixel is considered black; increasing this will treat dark greys as black",
+        default_value_t = 0.1,
+        value_parser = black_pixel_threshold_in_range
+    )]
+    black_pixel_threshold: f64,
+
+    #[arg(
+        short = 'w',
+        long = "windows",
+        help = "comma-separated list of second ranges; any black sections outside of these ranges will be ignored",
+        value_parser = window_parser,
+        value_delimiter = ','
+    )]
+    windows: Vec<Window>,
+}
+
+fn main() {
+    let args = Args::parse();
+    dbg!(args);
+}
+
+fn black_pixel_threshold_in_range(s: &str) -> Result<f64, String> {
+    validate_float(s, "black pixel threshold")
+}
+
+fn black_pixel_ratio_in_range(s: &str) -> Result<f64, String> {
+    validate_float(s, "ratio of black pixels")
+}
+
+fn validate_float(s: &str, description: &str) -> Result<f64, String> {
+    let value: f64 = s
+        .parse()
+        .map_err(|_| format!("`{s}` isn't a valid {description}"))?;
+
+    if value >= 0.0 && value <= 1.0 {
+        Ok(value)
+    } else {
+        Err(format!("{description} not in range 0-1"))
+    }
+}
---
+++ src/window.rs
@@ -0,0 +1,28 @@
+use std::time::Duration;
+
+#[derive(Debug, Clone)]
+pub struct Window {
+    start: Duration,
+    finish: Duration,
+}
+
+pub fn window_parser(s: &str) -> Result<Window, String> {
+    if s.contains(':') {
+        let spots: Vec<Duration> = s
+            .split(':')
+            .flat_map(|s| s.parse::<f64>().ok())
+            .map(Duration::from_secs_f64)
+            .collect();
+
+        if spots.len() != 2 {
+            Err("Invalid window".to_string())
+        } else {
+            Ok(Window {
+                start: spots[0],
+                finish: spots[1],
+            })
+        }
+    } else {
+        Err("Invalid window".to_string())
+    }
+}
